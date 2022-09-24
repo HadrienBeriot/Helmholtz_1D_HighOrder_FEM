6dbabf97b4e308e6df0088d7a5c762de41c21eb5
Jelle Langedijk <jellel@tutanota.com>
2022-09-24 22:43:55 +0200

    Indentation, Indexing Element
    Some parts of the last two method were wrongly indented, giving me some "red highlighted" code. That is now fixed, but purely cosmetic.
    
    I fixed a potential mistake in the `MassAndStiffness_1D` method. I changed Element[0, iElem-1] to Element[0, iElem] since we look at the current element & in the notebook `iElem` starts at 0. Using -1 here, in my understanding, results for the first iteration in selecting the last element, etc. I reran the whole notebook with the change in effect and it (still) shows correct results.

--- ExternalFunctions.py
+++ ExternalFunctions.py
@@ -1,4 +1,3 @@
-
 def Lobatto(xi, Order):
     import numpy as np
     Lo = np.zeros((np.size(xi), 9))
@@ -57,94 +56,82 @@ def CreateDofs(NrOfNodes, NrOfElem, Element, Order):
     return NrOfDofs, DofNode, DofElement
 
 def MassAndStiffness_1D(iElem, Order, Coord, Element):
-  import numpy as np
+    import numpy as np
 
-  Ke = np.zeros((Order+1, Order+1), dtype=np.complex128)
-  Me = np.zeros((Order+1, Order+1), dtype=np.complex128)
+    Ke = np.zeros((Order+1, Order+1), dtype=np.complex128)
+    Me = np.zeros((Order+1, Order+1), dtype=np.complex128)
 
-  x1 = Coord[Element[0, iElem-1].astype(int)]
-  x2 = Coord[Element[1, iElem-1].astype(int)]
-  Le = (x2-x1)
-  # quadrature rule (integration order 2*Order
-  GaussPoints, GaussWeights = np.polynomial.legendre.leggauss(int(2*Order))
-  NrOfGaussPoints = GaussWeights.size
-  # Loop over the Gauss points
-  for n in np.arange(0, NrOfGaussPoints):
-      xi = GaussPoints[n]
-      # High-order Lobatto shape functions
-      L = Lobatto(xi, Order)
-      dLdxi = LobattoDerivative(xi, Order)
-      dLdx = 2/Le*dLdxi
-      # Elementary matrices
-      Ke += GaussWeights[n]*np.outer(dLdx, dLdx)*Le/2
-      Me += GaussWeights[n]*np.outer(L, L)*Le/2
-  return Ke, Me
+    x1 = Coord[Element[0, iElem].astype(int)] #Changed Element[0, iElem-1] to Element[0, iElem] since we look at the current element & in the notebook `iElem` starts at 0...
+    x2 = Coord[Element[1, iElem].astype(int)] #Changed Element[0, iElem-1] to Element[0, iElem] since we look at the current element & in the notebook `iElem` starts at 0...
+    Le = (x2-x1)
+    # quadrature rule (integration order 2*Order
+    GaussPoints, GaussWeights = np.polynomial.legendre.leggauss(int(2*Order))
+    NrOfGaussPoints = GaussWeights.size
+    # Loop over the Gauss points
+    for n in np.arange(0, NrOfGaussPoints):
+        xi = GaussPoints[n]
+        # High-order Lobatto shape functions
+        L = Lobatto(xi, Order)
+        dLdxi = LobattoDerivative(xi, Order)
+        dLdx = 2/Le*dLdxi
+        # Elementary matrices
+        Ke += GaussWeights[n]*np.outer(dLdx, dLdx)*Le/2
+        Me += GaussWeights[n]*np.outer(L, L)*Le/2
+    return Ke, Me
 
 
 def GetSolutionOnSubgrid(Sol, Order, Coord, Element, NrOfElem, DofElement, NrOfWavesOnDomain):
-  # create a finer subgrid to interpolate the numerical solution
-  import numpy as np
-  # use a sampling of 20 points per wavelength for plotting
-  NrOfSubgridPoints = np.ceil(NrOfWavesOnDomain*20).astype(int)
-  #
-  xi = np.linspace(-1., 1., NrOfSubgridPoints)
-  L = Lobatto(xi, Order)
-  x_visu = np.zeros((NrOfSubgridPoints, NrOfElem))
-  u_visu = np.zeros((NrOfSubgridPoints, NrOfElem), dtype=np.complex128)
-  # Construct solution for each element
-  for iElem in np.arange(0, NrOfElem):
-    ElemDofs = DofElement[:, iElem]
-    # dofs of the element
-    x_nodes = Coord[Element[0: 2, iElem].astype(int)]
-    # geometrical "hat" shape functions
-    N1 = (1-xi)/2; N2 = (1+xi)/2
-    # mapping from reference to physical domaim
-    x_visu[:, iElem] = N1*x_nodes[0] + N2*x_nodes[1]
-    # Numerical solution on subgrid 
-    ElemSol = Sol[ElemDofs.astype(int)]
-    u_visu[:, iElem] = np.dot(L,ElemSol).flatten()
-
-  x_visu = x_visu.flatten('F')
-  u_visu = u_visu.flatten('F')
-  
-  return x_visu, u_visu
+    # create a finer subgrid to interpolate the numerical solution
+    import numpy as np
+    # use a sampling of 20 points per wavelength for plotting
+    NrOfSubgridPoints = np.ceil(NrOfWavesOnDomain*20).astype(int)
+    xi = np.linspace(-1., 1., NrOfSubgridPoints)
+    L = Lobatto(xi, Order)
+    x_visu = np.zeros((NrOfSubgridPoints, NrOfElem))
+    u_visu = np.zeros((NrOfSubgridPoints, NrOfElem), dtype=np.complex128)
+    # Construct solution for each element
+    for iElem in np.arange(0, NrOfElem):
+        ElemDofs = DofElement[:, iElem]
+        # dofs of the element
+        x_nodes = Coord[Element[0: 2, iElem].astype(int)]
+        # geometrical "hat" shape functions
+        N1 = (1-xi)/2; N2 = (1+xi)/2
+        # mapping from reference to physical domaim
+        x_visu[:, iElem] = N1*x_nodes[0] + N2*x_nodes[1]
+        # Numerical solution on subgrid 
+        ElemSol = Sol[ElemDofs.astype(int)]
+        u_visu[:, iElem] = np.dot(L,ElemSol).flatten()
+    x_visu = x_visu.flatten('F')
+    u_visu = u_visu.flatten('F')
+    return x_visu, u_visu
 
 
 def ComputeFullSolution(DuctLength,NrOfElem,Order,omega,rho0,c0,Vn,beta):
-  # performs the numerical simulation for these inputs
-  import numpy as np
-  import math
-  
-  # first create the mesh and the Dofs list
-  NrOfNodes, Coord, Element = Mesh1D(DuctLength,NrOfElem)
-  NrOfDofs, DofNode, DofElement = CreateDofs(NrOfNodes,NrOfElem,Element,Order)
-
-  Matrix = np.zeros((NrOfDofs,NrOfDofs), dtype=np.complex128)
-  Rhs = np.zeros((NrOfDofs,1), dtype=np.complex128)
-  for iElem in np.arange(0,NrOfElem):
-      # call the function returning the mass and stifness element matrices
-      Ke, Me = MassAndStiffness_1D(iElem, Order, Coord, Element)
-      ElemDofs = (DofElement[:,iElem]).astype(int)
-      # assemble - [side note "irregular slice" requires np.ix_ in python]
-      Matrix[np.ix_(ElemDofs,ElemDofs)] += Ke - (omega/c0)**2*Me
-
-  # now apply impedance boundary condition at last node
-  Matrix[DofNode[NrOfNodes-1],DofNode[NrOfNodes-1]] += 1j*omega/c0*beta
-
-  # and the velocity at first node 
-  Rhs[DofNode[0]] = 1j*omega*rho0*Vn
-
-  # solve the sparse system of equations 
-  Sol = np.linalg.solve(Matrix, Rhs) 
-  
-  # compute solution on subgrid
-  Lambda = 2*math.pi/(omega/c0); NrOfWavesOnDomain = DuctLength/Lambda
-  x_sub, u_h_sub = GetSolutionOnSubgrid(Sol, Order, Coord, Element, NrOfElem, DofElement, NrOfWavesOnDomain)
-  
-  # exact solution on subgrid 
-  u_exact_sub = np.exp(-1j*omega/c0*x_sub)
-
-  E2 = np.linalg.norm(u_h_sub - u_exact_sub)/np.linalg.norm(u_exact_sub)*100
-  #print('-' *100 +'\n'+' Numerical error is %1.4g' %E2 + ' % \n' +'-' *100)
-  
-  return E2, NrOfDofs
+    # performs the numerical simulation for these inputs
+    import numpy as np
+    import math
+    # first create the mesh and the Dofs list
+    NrOfNodes, Coord, Element = Mesh1D(DuctLength,NrOfElem)
+    NrOfDofs, DofNode, DofElement = CreateDofs(NrOfNodes,NrOfElem,Element,Order)
+    Matrix = np.zeros((NrOfDofs,NrOfDofs), dtype=np.complex128)
+    Rhs = np.zeros((NrOfDofs,1), dtype=np.complex128)
+    for iElem in np.arange(0,NrOfElem):
+        # call the function returning the mass and stifness element matrices
+        Ke, Me = MassAndStiffness_1D(iElem, Order, Coord, Element)
+        ElemDofs = (DofElement[:,iElem]).astype(int)
+        # assemble - [side note "irregular slice" requires np.ix_ in python]
+        Matrix[np.ix_(ElemDofs,ElemDofs)] += Ke - (omega/c0)**2*Me
+    # now apply impedance boundary condition at last node
+    Matrix[DofNode[NrOfNodes-1],DofNode[NrOfNodes-1]] += 1j*omega/c0*beta
+    # and the velocity at first node 
+    Rhs[DofNode[0]] = 1j*omega*rho0*Vn
+    # solve the sparse system of equations 
+    Sol = np.linalg.solve(Matrix, Rhs) 
+    # compute solution on subgrid
+    Lambda = 2*math.pi/(omega/c0); NrOfWavesOnDomain = DuctLength/Lambda
+    x_sub, u_h_sub = GetSolutionOnSubgrid(Sol, Order, Coord, Element, NrOfElem, DofElement, NrOfWavesOnDomain)
+    # exact solution on subgrid 
+    u_exact_sub = np.exp(-1j*omega/c0*x_sub)
+    E2 = np.linalg.norm(u_h_sub - u_exact_sub)/np.linalg.norm(u_exact_sub)*100
+    #print('-' *100 +'\n'+' Numerical error is %1.4g' %E2 + ' % \n' +'-' *100)
+    return E2, NrOfDofs
